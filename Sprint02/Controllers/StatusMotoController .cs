13f585f7a4c9b848a4bad0e30ea9bc784c4a4474
Sofia Andrade Petruk <157611475+sofiapetruk@users.noreply.github.com>
2025-09-29 20:18:03 -0300

    Update StatusMotoController .cs

--- Sprint02/Controllers/StatusMotoController .cs
+++ Sprint02/Controllers/StatusMotoController .cs
@@ -7,7 +7,7 @@ using Swashbuckle.AspNetCore.Annotations;
 namespace Sprint02.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/status-moto")]
     public class StatusMotoController : ControllerBase
     {
         private readonly IService<StatusMotoResponseDto, StatusMotoRequestDto> _service;
